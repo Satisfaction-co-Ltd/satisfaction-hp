849d4bb0ae4bc3199a4d6b97df01e1248e75a1f2
k-skjm000 <k-sekijima@satis-f.co.jp>
2026-02-03 19:06:06 +0900

    ブログ追加

---
+++ src/data/blogPosts.ts
@@ -0,0 +1,69 @@
+
+export interface BlogPost {
+  id: number;
+  title: string;
+  date: string;
+  image: string;
+  link: string;
+  summary: string;
+  content: string;
+  comments: number;
+}
+
+export const blogPosts: BlogPost[] = [
+  {
+    id: 1,
+    title: "noteとかの他のブログサイトの内容を表示することはできるか？",
+    date: "April 14, 2023",
+    image: "/images/blog/blog1.jpg",
+    link: "blog-2026-02-03-1",
+    summary: "",
+    content: `
+      <p>本文</p>
+      <h3>本文</h3>
+      <ul>
+        <li>本文</li>
+        <li>本文</li>
+        <li>本文</li>
+      </ul>
+    `,
+    comments: 0
+  },
+  {
+    id: 2,
+    title: "会社HP側で更新とかしなくても最新の内容を反映できるか？",
+    date: "April 13, 2023",
+    image: "/images/blog/blog2.jpg",
+    link: "blog-2026-02-03-2",
+    summary: "",
+    content: `
+      <p>本文</p>
+      <h3>本文</h3>
+      <ul>
+        <li>本文</li>
+        <li>本文</li>
+        <li>本文</li>
+      </ul>
+    `,
+    comments: 4
+  },
+  {
+    id: 3,
+    title: "できない場合はブログの更新方法を考えないといけない",
+    date: "April 13, 2023",
+    image: "/images/blog/blog2.jpg",
+    link: "blog-2026-02-03-3",
+    summary: "",
+    content: `
+      <p>本文</p>
+      <h3>本文</h3>
+      <ul>
+        <li>本文</li>
+        <li>本文</li>
+        <li>本文</li>
+      </ul>
+    `,
+    comments: 4
+  },
+  // ... 追加する場合はここに追加
+];
